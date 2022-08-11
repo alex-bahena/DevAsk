6a01ecba4b53004cf237c4915b6ca400cf080b42
javier fernandez laptop <zhuneslave@gmail.com>
2022-08-10 20:49:09 -0500

    gitignore dotenv

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,4 +1,12 @@
 const mongoose = require('mongoose');
+// require('dotenv').config();
+const path = require('path')
+require("dotenv").config({path: ".env"})
+// require('dotenv').config({ 
+//    path: path.resolve(__dirname, '../../.env') 
+// })
+
+
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb+srv://testUser:Holajavi1@cluster0.ajcc3kn.mongodb.net/?retryWrites=true&w=majority",
@@ -8,4 +16,26 @@ mongoose.connect(
   }
 );
 
+//mongoose.connect(
+  //     process.env.BBDD,
+  //      {
+  //     userNewUrlParser: true,
+  //     userUnifiedTopology: true,
+  //     userFindAndModify: true,
+  //     useCreateIndex: true
+  // },
+  // (err,_) =>{
+  //     if(err){
+  //         console.log("error de conexion");
+  //     } else {
+  //         console.log("conexion correcta");
+  //             server()
+  //     }
+  // }
+  // )
+
+
+
 module.exports = mongoose.connection;
+
+//"mongodb+srv://testUser:Holajavi1@cluster0.ajcc3kn.mongodb.net/?retryWrites=true&w=majority",
