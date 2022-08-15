c3434e19f48df74f1c345d63bfe1906cd904f23d
Adrian Strozzi <adrianstrozzi@gmail.com>
2022-08-15 17:27:07 -0500

    Added react-dropzone to upload new profile picture

--- client/src/components/User/AvatarForm/AvatarForm.js
+++ client/src/components/User/AvatarForm/AvatarForm.js
@@ -1,17 +1,31 @@
 
 import { Button } from "semantic-ui-react";
+import { useDropzone } from "react-dropzone";
+import React, { useCallback } from "react";
 import "./AvatarForm.scss";
 
 export default function AvatarForm(props) {
   const { setShowModal } = props;
 
+  const onDrop = useCallback((acceptedFile) => {
+    console.log(acceptedFile);
+  }, []);
+
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: "image/jpeg, image/png",
+    noKeyboard: true,
+    multiple: false,
+    onDrop,
+  });
+
 
   return (
 
     <div className="avatar-form">
-      <Button>Upload Photo</Button>
+      <Button {...getRootProps()}>Upload Photo</Button>
       <Button>Delete Current Photo</Button>
       <Button onClick={() => setShowModal(false)}>Cancel</Button>
+      <input {...getInputProps()} />
     </div>
   )
 
