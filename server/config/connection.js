a13fa3b0c2f1c46b11740c66079cec0dee6ca07a
Alejandro Bahena <alejandro.bahsan@gmail.com>
2022-08-17 22:35:32 -0500

    1

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,8 @@
 const mongoose = require("mongoose");
-require("dotenv").config({ path: __dirname + "/../../.env" });
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1/devask", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
 });
 
 module.exports = mongoose.connection;
