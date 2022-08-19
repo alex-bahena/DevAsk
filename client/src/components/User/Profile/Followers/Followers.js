3d3fb0171a8032023cd8e0dcf44c6d6f6c83adda
Javier R <javierresines@gmail.com>
2022-08-18 21:16:06 -0500

    debugging and working on follower count

---
+++ client/src/components/User/Profile/Followers/Followers.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Followers.scss";
+
+export default function Followers(props) {
+    const { username } = props;
+  return (
+    <div classname="followers">
+<h2> estamos en followers</h2>
+    </div>
+  );
+}
