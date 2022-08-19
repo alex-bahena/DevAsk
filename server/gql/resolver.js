224b03c04165e68256d30b5ea64e5d0fd4d815ef
Alejandro Bahena <alejandro.bahsan@gmail.com>
2022-08-18 21:59:41 -0500

    typo

--- server/gql/resolver.js
+++ server/gql/resolver.js
@@ -1,26 +1,32 @@
-// const { UserInputError } = require("apollo-server-express");
+// const { AuthenticationError } = require("apollo-server-express");
 const userController = require("../controllers/user");
-const { GraphQLUpload } = require("graphql-upload")
+const { GraphQLUpload } = require("graphql-upload");
 const followController = require("../controllers/follow");
 // const user = require("../models/user");
 
 const resolvers = {
-    Upload: GraphQLUpload,
-    Query: {
-        getUser: (_, { id, username }) => { return userController.getUser(id, username) },
-        search: (_, { search }) => userController.search(search),
+  Upload: GraphQLUpload,
+  Query: {
+    getUser: (_, { id, username }) => {
+      return userController.getUser(id, username);
     },
+    search: (_, { search }) => userController.search(search),
+  },
 
-    Mutation: {
-        register: (_, { input }) => { userController.register(input) },
-        login: (_, { input }) => { return userController.login(input) },
-        updateAvatar: (_, { file }, ctx) => userController.updateAvatar(file, ctx),
-        deleteAvatar: (_, { }, ctx) => userController.deleteAvatar(ctx),
-        updateUser: (_, { input }, ctx) => userController.updateUser(input, ctx),
+  Mutation: {
+    register: (_, { input }) => {
+      userController.register(input);
+    },
+    login: (_, { input }) => {
+      return userController.login(input);
+    },
+    updateAvatar: (_, { file }, ctx) => userController.updateAvatar(file, ctx),
+    deleteAvatar: (_, {}, ctx) => userController.deleteAvatar(ctx),
+    updateUser: (_, { input }, ctx) => userController.updateUser(input, ctx),
 
-        //Follow
-        follow: (_, { username }, ctx) => followController.follow(username, ctx),
-    }
-}
+    //Follow
+    follow: (_, { username }, ctx) => followController.follow(username, ctx),
+  },
+};
 
-module.exports = resolvers
+module.exports = resolvers;
