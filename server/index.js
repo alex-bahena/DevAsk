06e98a0714da70d61364e2105528e4873aeb2298
Alejandro Bahena <alejandro.bahsan@gmail.com>
2022-08-20 11:42:18 -0500

    1 version back

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static("../client/build"));
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
