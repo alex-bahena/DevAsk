f924274ef6463d4662451abfe2fd32077fc5e9f8
Alejandro Bahena <alejandro.bahsan@gmail.com>
2022-08-19 21:23:04 -0500

    heroku - new test.

--- server/index.js
+++ server/index.js
@@ -15,10 +15,6 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-}
-
 const corsOptions = {
   origin: "*",
   credentials: true, //access-control-allow-credentials:true
