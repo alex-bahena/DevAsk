fda12ca0601c3355f451e9a50884fe9e773add70
Alejandro Bahena <alejandro.bahsan@gmail.com>
2022-08-20 01:03:38 -0500

    mutation

--- server/gql/resolver.js
+++ server/gql/resolver.js
@@ -2,44 +2,39 @@
 const userController = require("../controllers/user");
 const { GraphQLUpload } = require("graphql-upload");
 const followController = require("../controllers/follow");
-const publicationController = require("../controllers/publication")
+const publicationController = require("../controllers/publication");
 // const user = require("../models/user");
 
 const resolvers = {
-
-    Upload: GraphQLUpload,
-    Query: {
-        getUser: (_, { id, username }) => { return userController.getUser(id, username) },
-        search: (_, { search }) => userController.search(search),
-
-        //FOLLOW
-        isFollow: (_, { username }, ctx) =>
-            followController.isFollow(username, ctx),
-            getFollowers: (_, {username}) => followController.getFollowers(username),
-
+  Upload: GraphQLUpload,
+  Query: {
+    getUser: (_, { id, username }) => {
+      return userController.getUser(id, username);
     },
     search: (_, { search }) => userController.search(search),
+
+    //FOLLOW
+    isFollow: (_, { username }, ctx) =>
+      followController.isFollow(username, ctx),
+    getFollowers: (_, { username }) => followController.getFollowers(username),
   },
 
   Mutation: {
-    register: (_, { input }) => {
-      userController.register(input);
-    },
-    login: (_, { input }) => {
-      return userController.login(input);
-    },
+    // User
+    register: (_, { input }) => userController.register(input),
+    login: (_, { input }) => userController.login(input),
     updateAvatar: (_, { file }, ctx) => userController.updateAvatar(file, ctx),
     deleteAvatar: (_, {}, ctx) => userController.deleteAvatar(ctx),
     updateUser: (_, { input }, ctx) => userController.updateUser(input, ctx),
 
+    // Follow
+    follow: (_, { username }, ctx) => followController.follow(username, ctx),
+    unFollow: (_, { username }, ctx) =>
+      followController.unFollow(username, ctx),
 
-        //Follow
-        follow: (_, { username }, ctx) => followController.follow(username, ctx),
-        unFollow: (_, { username }, ctx) => followController.unFollow(username, ctx),
-
-        //Publication
-        publish: (_, { file }, ctx) => publicationController.publish(file, ctx),
-    },
-}
+    // Publication
+    publish: (_, { file }, ctx) => publicationController.publish(file, ctx),
+  },
+};
 
 module.exports = resolvers;
